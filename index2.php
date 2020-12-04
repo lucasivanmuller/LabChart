67b8310baf219c920075dd1f4aeaa11239df4c07
lucasivanmuller <44439953+lucasivanmuller@users.noreply.github.com>
2020-12-04 19:03:43 -0300

    Add files via upload

--- index2.php
+++ index2.php
@@ -1,12 +1,13 @@
 <?php
 
-        $debugging = true;
+        $debugging = false;
         $agrupar_estudios_array = array(
             'Hemograma' => array('HTO', 'HGB', 'LEU','NSEG', 'CAY',  'LIN', 'PLLA'),
             'Medio interno' => array('KAS', 'NAS'),
-            'FunciÃ³n renal' => array('URE', 'CRE'),
+            'Función renal' => array('URE', 'CRE'),
             'Excluir' => array('BAS', 'EOS', 'META', 'MI', 'MON', 'PRO', 'SB', 'SUMAL')
         );
+
         function pacientes_por_piso($piso) {	
             global $debugging;
             $pacientes_array = array();
@@ -16,7 +17,7 @@
                 $pacientes_raw = json_decode(file_get_contents("http://172.24.24.131:8007/html/internac.php?funcion=pacientes&piso=".$piso), true);
             }
             foreach ($pacientes_raw['pacientes'] as $paciente) {
-                $pacientes_array[] = array('HC' => $paciente['pacientes']['hist_clinica'], "Nombre" => $paciente['pacientes']['apellido1'].", ".$paciente['pacientes']['nombre'],"Cama" => $paciente['pacientes']['cama'], "Ordenes" => array());
+                $pacientes_array[] = array('HC' => $paciente['pacientes']['hist_clinica'], "Nombre" => $paciente['pacientes']['apellido1'].", ".$paciente['pacientes']['nombre'],"Cama" => $paciente['pacientes']['cama']);
             }
             return $pacientes_array;
 	}
@@ -30,7 +31,11 @@
             $ordenes_raw = json_decode(file_get_contents("http://172.24.24.131:8007/html/internac.php?funcion=ordenestot&HC=".$HC), true);
         }
 		foreach ($ordenes_raw['ordenestot'] as $orden)	{
-			$ordenes_array[] = array("n_solicitud" => $orden['ordenestot']['NRO_SOLICITUD'], "timestamp" => $orden['ordenestot']['RECEPCION']);
+			$fecha_labo = substr($orden['ordenestot']['RECEPCION'], 0, 10);
+			if ($fecha_labo == date('d/m/Y')) {
+				$ordenes_array[] = array("n_solicitud" => $orden['ordenestot']['NRO_SOLICITUD'], "timestamp" => $orden['ordenestot']['RECEPCION']);
+			}
+
 		}
 		return $ordenes_array;
 	}
@@ -48,7 +53,7 @@
             }
             $estudio_array['orden'] = $orden;
             
-            #Agrupa cada resultado del laboratorio segÃºn los grupos definidos en $agrupar_estudios_array (ej: Hemograma, hepatograma, etc)
+            #Agrupa cada resultado del laboratorio según los grupos definidos en $agrupar_estudios_array (ej: Hemograma, hepatograma, etc)
             foreach ($estudio_raw['estudiostot'] as $estudio) {	
                 $codigo = $estudio['estudiostot']['CODANALISI']; 
                 if (in_array($codigo, $agrupar_estudios_array['Excluir'])) {
@@ -69,7 +74,7 @@
                         break;
                     }
                 }
-                if (!$categoria_encontrada) { #Si no entra en ninguna categorÃ­a preestablecida, va a "otros"
+                if (!$categoria_encontrada) { #Si no entra en ninguna categoría preestablecida, va a "otros"
                     $estudio_array['Otros'][$codigo] = array(
                     'nombre_estudio' => $estudio['estudiostot']['NOMANALISIS'], 
                     'resultado' => $estudio['estudiostot']['RESULTADO'],
@@ -81,14 +86,26 @@
 	}
 
 $array_final = array();
-        
-$pacientes_array = pacientes_por_piso(7);
+$pacientes = pacientes_por_piso($_GET['piso']);
+foreach ($pacientes as $paciente) {
+	foreach(ordenes_de_paciente($paciente['HC']) as $orden) {
+		$resultado = procesar_estudio($orden['n_solicitud']);
+		$array_final[] = array_merge($paciente, $resultado);
+	}
+}
 
-foreach ($pacientes_array as $paciente) {
-    $HC = $paciente['HC'];
-    foreach (ordenes_de_paciente($HC) as $orden) {
-        $array_final[$orden] = procesar_estudio($orden);
+function utf8ize($d) {
+        if (is_array($d)) {
+            foreach ($d as $k => $v) {
+                $d[$k] = utf8ize($v);
+            }
+        } else if (is_string ($d)) {
+            return utf8_encode($d);
+        }
+        return $d;
     }
-}   
-echo json_encode($array_final);
+
+$value = utf8ize($array_final);
+echo json_encode(utf8_encode($value));
+echo json_last_error_msg();
 ?>
